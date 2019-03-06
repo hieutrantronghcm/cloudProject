4a7e6f00f6c7b8a0716937653a8fa654bc86838d
hieutrantronghcm <hieutrantronghcm@gmail.com>
2019-03-06 20:07:52 +0700

    no message

---
+++ untitled/src/components/SearchBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-form>
+      <br>
+      <v-flex style="display: inline-flex">
+        <v-text-field v-model="searchValue" solo label="Search product" style="width: 500px"/>
+        <v-btn style="float: left;">Search</v-btn>
+      </v-flex>
+    </v-form>
+</template>
+
+<script>
+  import axios from 'axios'
+    export default {
+        name: "SearchBar",
+      data() {
+          return {
+            searchValue: ''
+          }
+      },
+      methods: {
+          searchByName() {
+            axios.get('http://localhost:8080/products/all', {
+              headers: {
+                // "Authorization": `Bearer ${localStorage.getItem("cdpmToken")}`,
+                "Content-type": 'application/json'
+              }
+            }).then(response => {
+              this.products = response.data;
+            }).catch(
+              () => console.log("Cannot found any result!")
+            )
+          }
+      }
+    }
+</script>
+
+<style scoped>
+#txtSearch {
+  width: 400px;
+}
+</style>
---
+++ untitled/src/views/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <login-form></login-form>
+  </div>
+
+</template>
+
+<script>
+    import LoginForm from "@/components/LoginForm";
+    import SearchBar from "@/components/SearchBar";
+    export default {
+        name: "LoginPage",
+      components: {LoginForm}
+    }
+</script>
+
+<style scoped>
+
+</style>
